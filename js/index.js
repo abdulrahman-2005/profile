9054212d0a53f7c310369aebde38a11ec7bc55ea
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2023-04-20 03:12:53 +0200

    New PUp

--- js/index.js
+++ js/index.js
@@ -1,184 +1,132 @@
-let currentTheme;
-let currentLanguage;
-window.onload = () => {
-	const HTMLEl = document.getElementsByTagName("html")[0];
-	try {
-		currentTheme = JSON.parse(
-			localStorage.getItem("__AbdulrahmanAzmyWebsiteStorage__")
-		).mode;
-	} catch (error) {
-		currentTheme = "dark";
-		currentLanguage = "en";
-	}
-	if (currentTheme === undefined) {
-		currentTheme = "dark";
-	}
-	try {
-		currentLanguage = JSON.parse(
-			localStorage.getItem("__AbdulrahmanAzmyWebsiteStorage__")
-		).language;
-	} catch (error) {
-		currentLanguage = "en";
-	}
-	localStorage.setItem(
-		"__AbdulrahmanAzmyWebsiteStorage__",
-		JSON.stringify({
-			mode: currentTheme,
-			language: currentLanguage,
-		})
-	);
-	HTMLEl.setAttribute("data-theme", currentTheme);
-	
-	let changeModeButton = document.getElementById("modeChange");
-	
-	changeModeButton.innerHTML = currentTheme === "dark" ? "☀" : "🌙";
-	changeModeButton.setAttribute(
-		"data-tooltip",
-		currentTheme === "dark" ? "Light Mode" : "Dark Mode"
-	);
-	changeModeButton.setAttribute("data-placement", "bottom");
+const htmlEl = document.querySelector('html');
+const headEl = document.querySelector('head');
+const containerEl = document.getElementById('container');
+const modeChangeButton = document.getElementById('modeChange');
+const changeLanguageButton = document.getElementById('changeLanguage');
+const messageTextarea = document.getElementById('message-textarea');
 
-	const headEl = document.getElementsByTagName("head")[0];
-
-};
-
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const englishJokeMessages = [
+  'stop it',
+  'you\'re annoying',
+  'stop',
+  'what did you think would happen?',
+  'just don\'t',
+  'can you stop clicking any button?',
+  'why are you doing this?',
+  'why are you doing this to me?',
+  'was it worth it?',
+  'you\'re a bad person',
+  'heck no',
+  'haha, stop it',
+];
+const arabicJokeMessages = [
+  'بس خلاص',
+  'كفاية بقى',
+  'مش كل شوية',
+  'يعم صل عالنبي',
+  'بطل تدوس الزراير',
+  'كان هيحصل ايه يعني 🤣🤣',
+  'طبعا لازم البيه',
+  'بس',
+  'كفاية',
+  'بطل تدوس زرايري',
+];
 
 class LocalStorageAccess {
-	constructor(StorageName) {
-		this._storage = window.localStorage;
-		this._storageName = StorageName;
-		try {
-			this._storage.getItem(this._storageName);
-		} catch (error) {
-			this._storage.setItem(
-				this._storageName,
-				JSON.stringify({
-					language: "en",
-					theme: "dark",
-				})
-			);
-		}
-	}
+  constructor(storageName) {
+    this.storage = window.localStorage;
+    this.storageName = storageName;
+    try {
+      this.storage.getItem(this.storageName);
+    } catch (error) {
+      this.storage.setItem(this.storageName, JSON.stringify({
+        language: 'en',
+        theme: 'dark',
+      }));
+    }
+  }
 
-	get() {
-		return JSON.parse(this._storage.getItem(this._storageName));
-	}
+  get() {
+    return JSON.parse(this.storage.getItem(this.storageName));
+  }
 
-	set(value) {
-		this._storage.setItem(this._storageName, JSON.stringify(value));
-	}
+  set(value) {
+    this.storage.setItem(this.storageName, JSON.stringify(value));
+  }
 }
 
-MyWebsiteStorage = new LocalStorageAccess("__AbdulrahmanAzmyWebsiteStorage__");
+const myWebsiteStorage = new LocalStorageAccess('AbdulrahmanAzmyWebsiteStorage');
 
-const Styler = (stylerName) => {
-	const style = `<style id="${stylerName}"></style>`;
-	document.head.innerHTML += style;
-	return document.getElementById(stylerName);
-};
+function setTheme(theme) {
+  htmlEl.setAttribute('data-theme', theme);
+  modeChangeButton.innerHTML = theme === 'dark' ? '☀' : '🌙';
+  modeChangeButton.setAttribute('data-tooltip', theme === 'dark' ? 'Light Mode' : 'Dark Mode');
+  myWebsiteStorage.set({ ...myWebsiteStorage.get(), mode: theme });
+}
 
-flashyBorderStyler = Styler("flashyBorderStyler");
-const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+function toggleTheme() {
+  const theme = htmlEl.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+  setTheme(theme);
+}
+
+function setJokeMessage() {
+  const language = myWebsiteStorage.get().language;
+  const joke = language === 'en' ? englishJokeMessages[Math.floor(Math.random() * englishJokeMessages.length)] : arabicJokeMessages[Math.floor(Math.random() * arabicJokeMessages.length)];
+  const old = containerEl.innerHTML;
+  containerEl.innerHTML = `<article class="joke-message" style="border-radius: 40px;padding: 2vw; font-weight: bold;font-size: 1.2rem; background:var(--flash-border-color); color: white;">${joke}</article>`;
+  setTimeout(() => {
+    containerEl.innerHTML = old;
+  }, 2000);
+}
+
+function toggleLanguage() {
+  const oldStorage = myWebsiteStorage.get();
+  const language = oldStorage.language === 'en' ? 'ar' : 'en';
+  myWebsiteStorage.set({ ...oldStorage, language });
+  window.location.href = language === 'en' ? '../en' : '../ar';
+}
+
+function sendMessage(option = 'whatsapp') {
+  let message = messageTextarea.value;
+  message = message === '' ? defalutMessage[myWebsiteStorage.get().language.toLowerCase()] : message;
+  if (option === 'whatsapp') {
+    message = message.replace(/ /g, '-');
+    window.open(`https://wa.me/201201590033?text=${message}`, '_blank');
+  } else if (option === 'email') {
+    message = message.replace(/ /g, '20%');
+    window.open(`mailto:abuazmy.gg@gmail.com?subject=ContactFromWebsite&body=${message}`, '_blank');
+  }
+}
+
+function createStyler(stylerName) {
+  const styleEl = document.createElement('style');
+  styleEl.id = stylerName;
+  headEl.appendChild(styleEl);
+  return styleEl;
+}
+
+const flashyBorderStyler = createStyler('flashyBorderStyler');
 function flash() {
-	flashyBorderStyler.innerHTML = `
-	:root {
-		--flash-border-color: ${colors[Math.floor(Math.random() * colors.length)]
-		} !important;
-	}`;
+  flashyBorderStyler.innerHTML = `:root { --flash-border-color: ${colors[Math.floor(Math.random() * colors.length)]} !important; }`;
 }
 
 setInterval(flash, 1000);
 
-const englishJokeMessages = [
-	"stop it",
-	"you're annoying",
-	"stop",
-	"what did you think would happen?",
-	"just don't",
-	"can you stop clicking any button?",
-	"why are you doing this?",
-	"why are you doing this to me?",
-	"was it worth it?",
-	"you're a bad person",
-	"heck no",
-	"haha, stop it",
-];
-
-const arabicJokeMessages = [
-	"بس خلاص",
-	"كفاية بقى",
-	"مش كل شوية",
-	"يعم صل عالنبي",
-	"بطل تدوس الزراير",
-	"كان هيحصل ايه يعني 🤣🤣",
-	"طبعا لازم البيه",
-	"بس",
-	"كفاية",
-	"بطل تدوس زرايري",
-];
-function jokeMessage() {
-	let joke =
-		MyWebsiteStorage.get().language === "en"
-			? englishJokeMessages[
-			Math.floor(Math.random() * englishJokeMessages.length)
-			]
-			: arabicJokeMessages[
-			Math.floor(Math.random() * arabicJokeMessages.length)
-			];
-	let old = document.getElementById("container").innerHTML;
-	document.getElementById(
-		"container"
-	).innerHTML = `<article style="border-radius: 40px;padding: 2vw; font-weight: bold;font-size: 1.2rem; background:var(--flash-border-color); color: white;">${joke}</article>`;
-	setTimeout(() => {
-		document.getElementById("container").innerHTML = old;
-	}, 2000);
-}
-
-let oldStorage;
-function changeLanguage() {
-	oldStorage = MyWebsiteStorage.get();
-	oldStorage.language = oldStorage.language === "en" ? "ar" : "en";
-	MyWebsiteStorage.set(oldStorage);
-	window.location.href = oldStorage.language === "en" ? "../en" : "../ar";
-	let changeLanguageButton = document.getElementById("changeLanguage");
-}
-
-function changeMode(button) {
-	oldStorage = MyWebsiteStorage.get();
-	let htmlEl = document.getElementsByTagName("html")[0];
-	htmlEl.setAttribute(
-		"data-theme",
-		htmlEl.getAttribute("data-theme") === "dark" ? "light" : "dark"
-	);
-	button.innerHTML =
-		htmlEl.getAttribute("data-theme") === "dark" ? "☀" : "🌙";
-	oldStorage.mode = htmlEl.getAttribute("data-theme");
-	button.setAttribute(
-		"data-tooltip",
-		oldStorage.mode === "dark" ? "Light Mode" : "Dark Mode"
-	);
-	MyWebsiteStorage.set(oldStorage);
-}
-
-let defalutMessage = {
-	ar: "مرحبا، اريد اتحدث معك",
-	en: "Hello, I want to talk to you",
-};
-function sendMessage(option = "whatsapp") {
-	let message = document.getElementById("message-textarea").value;
-	message =
-		message === ""
-			? defalutMessage[MyWebsiteStorage.get().language.toLowerCase()]
-			: message;
-
-	if (option === "whatsapp") {
-		message = message.replace(/ /g, "-");
-		window.open(`https://wa.me/201201590033?text=${message}`, "_blank");
-	} else if (option === "email") {
-		message = message.replace(/ /g, "20%");
-		window.open(
-			`mailto:abuazmy.gg@gmail.com?subject=ContactFromWebsite&body=${message}`,
-			"_blank"
-		);
-	}
-}
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    const { mode, language } = myWebsiteStorage.get();
+    setTheme(mode || 'dark');
+    myWebsiteStorage.set({ ...myWebsiteStorage.get(), language });
+  } catch (error) {
+    setTheme('dark');
+    myWebsiteStorage.set({ ...myWebsiteStorage.get(), language: 'en' });
+  }
+  modeChangeButton.addEventListener('click', toggleTheme);
+  changeLanguageButton.addEventListener('click', toggleLanguage);
+  messageTextarea.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      sendMessage('whatsapp');
+    }
+  });
+});
