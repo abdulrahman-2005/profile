8c5f70939a8e5621e680ed9d87e493c879decfe5
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2023-08-31 17:01:53 +0200

    added grid display as the display for the projects_display, icons for each project dynamic, bg for each project random

--- js/projects_display.js
+++ js/projects_display.js
@@ -17,23 +17,33 @@ let displayTitles = {
 	}
 }[lang]
 
-console.log(data)
+
+function randomBGImage() {
+	return `bg${Math.floor(Math.random() * 8)+1}.svg`
+}
 
 function displayProject(project) {
     projectsListContainer.innerHTML += `
-    <article>
-		<h2>${project["name"]}</h2>
-		<details style="width: 100%">
-			<summary>${displayTitles["desc"]}</summary>
-			<p>
-                ${project["desc"]}
-			</p>
-		</details>
-		<div class="flexButtonContainer">
-			<a target=_blank href="${project["url"]}" role="button" style="width: 100%">${displayTitles["visit"]}</a>
-			<a target=_blank href="https://www.github.com/abdulrahman-2005/${project["github_repo"]}" role="button" style="width: 100%">${displayTitles["code"]}</a>
-		</div>
-	</article>
+    <article class="project-card" style="background-image: url('../../data/images/${randomBGImage()}');">
+				<div class="header">
+					<h2>${project["name"]}</h2>
+					<div class="img-container" >
+						<img src="../../data/images/${project["github_repo"]}.jpg">
+					</div>
+				</div>
+				<details style="width: 100%">
+					<summary>${displayTitles["desc"]}</summary>
+					<p>
+						${project["desc"]}
+					</p>
+				</details>
+				<div class="flexButtonContainer">
+					<a target=_blank href="${project["url"]}" role="button"
+						style="width: 100%">${displayTitles["visit"]}</a>
+					<a target=_blank href="https://www.github.com/abdulrahman-2005/${project[" github_repo"]}"
+						role="button" style="width: 100%">${displayTitles["code"]}</a>
+				</div>
+			</article>
             `
 }
 
@@ -43,5 +53,6 @@ function displayProjects(data) {
         displayProject(project)
     }
 }
+
 
 displayProjects(data)
