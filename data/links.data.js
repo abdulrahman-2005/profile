86a93c2d325a4654d1b9d1327e77ebbbcf84e954
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2023-09-01 14:15:24 +0200

    just testing the new design on mobile

---
+++ data/links.data.js
@@ -0,0 +1,50 @@
+const linksData = [
+	{
+		name: {
+			ar: "تويتر",
+			en: "twitter",
+		},
+		href: "https://twitter.com/Abdulra42082285",
+		src: "../icons/twitter.png",
+	},
+	{
+		name: {
+			ar: "فيسبوك",
+			en: "facebook",
+		},
+		href: "https://www.facebook.com/abdulrahman2oo5/",
+		src: "../icons/facebook.png",
+	},
+	{
+		name: {
+			ar: "جيت هاب",
+			en: "github",
+		},
+		href: "https://github.com/abdulrahman-2005/",
+		src: "../icons/github.png",
+	},
+	{
+		name: {
+			ar: "لينكد إن",
+			en: "linkedin",
+		},
+		href: "https://www.linkedin.com/in/abdulrahman-azmy-1129ab212/",
+		src: "../icons/linkedin.png",
+	},
+	{
+		name: {
+			ar: "واتساب",
+			en: "whatsapp",
+		},
+		href: "https://wa.me/201201590033?text=hello%2C%20let%20me%20introduce%20my%20self",
+		src: "../icons/whatsapp.png",
+	},
+	{
+		name: {
+			ar: "ريديت",
+			en: "reddit",
+		},
+		href: "https://www.reddit.com/user/AbdoAzmy",
+		src: "../icons/reddit.png",
+	},
+];
